c98d9fc202ae1a8f0ee46ae5993dc34ca2f2a19a
Cosmin <cosmin.p420@gmail.com>
2021-02-16 00:24:26 +0100

    s

--- page1.php
+++ page1.php
@@ -12,11 +12,11 @@ if($conn->connect_error){
     die("Connessione fallita: ".$conn->connect_error);
 }
 
-echo "connessione avvenuta con successo!\n"."1<br>2";
+echo "connessione avvenuta con successo!"."<br>";
 
 $sql = "select name from dati";
 $result = $conn->querry($sql);
-
+echo "1";
 if ($result->num_rows > 0) {
     
     while($row = $result->fetch_assoc()) {
@@ -25,6 +25,8 @@ if ($result->num_rows > 0) {
   } else {
     echo "0 results";
   }
+
+echo "2";
   $conn->close();
 
 
