44f7e0f4b0d754d4fd9b37a4d77717dc39a79803
Cosmin <cosmin.p420@gmail.com>
2021-02-15 23:37:19 +0100

    prova

--- page1.php
+++ page1.php
@@ -1,13 +1,41 @@
 <?php
 
 
-echo "ciao";
+$servername = "localhost";
+$username = "root";
+$password = "Cosmin13database";
+$dbname = "prova";
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connect_error){
+    die("Connessione fallita: ".$conn->connect_error);
+}
+
+echo "connessione avvenuta con successo!\n"."1<br>2";
+
+$sql = "select name from dati";
+$result = $conn->querry($sql);
+
+if ($result->num_rows > 0) {
+    
+    while($row = $result->fetch_assoc()) {
+      echo "NOME: " . $row["name"]."<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+  $conn->close();
+
+
+
+
 
 echo "<html>";
 
 
 
-echo "<a style='font-size=300;'>COSMIN</a>";
+
 
 
 
